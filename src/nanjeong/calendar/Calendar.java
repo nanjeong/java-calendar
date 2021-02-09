c71c84ff612bbafdafac8c1df5f97d54164da57c
nanjeong <whdwkxj7@naver.com>
2021-02-09 20:47:04 +0900

    Update Calendar.java 캘린더 출력

--- src/nanjeong/calendar/Calendar.java
+++ src/nanjeong/calendar/Calendar.java
@@ -2,6 +2,16 @@ package nanjeong.calendar;
 
 public class Calendar {
 	public static void main(String[] args) {
-		System.out.println("Hello, Calendar");
+		String[] week = {"일", "월", "화", "수", "목", "금", "토"};
+		for (int i = 0; i < 7; i++) {
+			System.out.printf("%5s", week[i]);
+		}
+		System.out.print("\n----------------------");
+		for (int i = 1; i <= 31; i++) {
+			if ((i-1) % 7 == 0) {
+				System.out.println();
+			}
+			System.out.printf("%3d", i);
+		}
 	}
 }
